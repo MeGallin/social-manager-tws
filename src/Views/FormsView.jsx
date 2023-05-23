6f7e92a16ceb7ea6862d361fc241dd38b87f0961
MeGallin <me@garyallin.uk>
2023-05-23 10:27:34 +0100

    baisc routing setup

---
+++ src/Views/FormsView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { LoginComponent, RegisterComponent } from '../Components';
+
+const formsView = () => {
+  return (
+    <>
+      <h1>formsView</h1>
+      <LoginComponent />
+      <RegisterComponent />
+    </>
+  );
+};
+
+export default formsView;
